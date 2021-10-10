b66ab73df069743213f3c40cf63ab6b958e4632f
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-10-10 19:36:00 +0200

    the getLocationData now returns a promise of ALL data for a location. Also can take location name from a search bar.

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 //Create a function that fetches name and temperature about a location
 async function getLocationData() {
     try {
-        const locationData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Poznan&units=metric&appid=052797962918a845d445305c6b4b4a78`, {mode: 'cors'});
+        const locationName = locationFind();
+        const locationData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&units=metric&appid=052797962918a845d445305c6b4b4a78`, {mode: 'cors'});
         const locationInfo = await locationData.json();
-        const locationTemp = locationInfo.main.temp;
-        return locationTemp;
+        return locationInfo;
         } catch (error){
             console.log(error);
         }
 }
 
 //Create a function that tracks for location value
-
+const locationFind = (() => {
+    let name = document.querySelector('input[type="search"]').value;
+    return name;
+});
 //On submit display info
+document.querySelector('form').addEventListener('submit', e => {
+    getLocationData();
+    console.log(getLocationData());
+    e.preventDefault();
+})
