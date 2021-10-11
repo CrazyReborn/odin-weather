112547df126c617fa0c87f12e28a2d9768c92b48
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-10-11 14:25:26 +0200

    Added a button for changing temp units. Changed img src to be from openwetaher api (this way icons are in a better spot). Also added some basic styling.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 async function getLocationData() {
     try {
         const locationName = locationFind();
-        const locationData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&units=metric&appid=052797962918a845d445305c6b4b4a78`, {mode: 'cors'});
+        const locationData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&units=${units}&appid=052797962918a845d445305c6b4b4a78`, {mode: 'cors'});
         const locationInfo = await locationData.json();
         return locationInfo;
         } catch (error){
@@ -10,18 +10,34 @@ async function getLocationData() {
         }
 }
 
+/* GET IMAGE FROM GIPHLY
 async function getImage () {
     try {
-        const gif = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=KrQBTKUAQewMdpvUMjmStSd5z8mAYw4D&weirdness=0&s=${weather}_weather_outside`, {mode: 'cors'});
+        const gif = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=KrQBTKUAQewMdpvUMjmStSd5z8mAYw4D&weirdness=0&s=${weather}_weather_icon`, {mode: 'cors'});
         const data = await gif.json();
         return data;
     } catch (error) {
         console.log(error);
     }
 }
+*/
 
-
+let units = 'metric';
 let weather;
+
+//Change metric
+const changeMetric = (() => {
+    document.querySelector('button').addEventListener('click', e=> {
+        if(units != 'imperial') {
+            units = 'imperial';
+        } else {
+            units = 'metric';
+        }
+        displayInfo();
+    })
+})();
+
+
 //Create a function that tracks for location value
 const locationFind = (() => {
     let name = document.querySelector('input[type="search"]').value;
@@ -30,7 +46,7 @@ const locationFind = (() => {
 //On submit display info
 document.querySelector('form').addEventListener('submit', e => {
     displayInfo();
-    displayImage();
+    //displayImage();       DISABLED BECAUSE OF HOW GIFS ARE RANDOM
     e.preventDefault();
 })
 
@@ -51,13 +67,24 @@ async function displayInfo () {
     weather = info.weather[0].main;
 
     locationNameOnPage.textContent += infoName;
-    locationTempOnPage.textContent += infoTemp + ' Cel';
+    if (units == 'metric') {
+        locationTempOnPage.textContent += infoTemp + ' Cel';
+    }
+    else {
+        locationTempOnPage.textContent += infoTemp + ' Fahrenheit';
+    }
+
+    const imageOnPage = document.querySelector('img');
+    const image = info.weather[0].icon;
+    imageOnPage.src = '';
+    imageOnPage.src = `https://openweathermap.org/img/wn/${image}@2x.png`;
 }
 
-//Display image
+/*Display image from gihply
 async function displayImage() {
     const imageOnPage = document.querySelector('img');
     const image = await getImage();
     imageOnPage.src = '';
     imageOnPage.src = image.data.images.downsized_medium.url
 }
+*/
